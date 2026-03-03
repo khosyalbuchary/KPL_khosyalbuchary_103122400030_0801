dea947821a4a08e0d1963c91383c3408239ea8dd
Khosyz <khosy021@gmail.com>
2026-03-04 00:58:40 +0700

    Update index.js

--- 02_Pemograman_JavaScript/index.js
+++ 02_Pemograman_JavaScript/index.js
@@ -1,16 +1,14 @@
-const arr1 = [1, -2, 3, -4, 5, -6];
+const arr1 = [2, 0, 26, 28, -2];;
 
 function mulOfArray(arr) {
     let result = 1;
-
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] > 0) {
             result = result * arr[i];
         }
     }
-
     return result;
 }
 
-const arrResult = mulOfArray(arr1);
-console.log(arrResult);
+const arr1Result = mulOfArray(arr1);
+console.log(arr1Result);
